3af39d9704c3e24715ba632bc096af42b2c2034c
pingme <62752555+L4ns@users.noreply.github.com>
2024-03-28 07:58:16 +0700

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,10 +3,10 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<a href="https://explorer-testnet.morphl2.io/" target={"https://explorer-testnet.morphl2.io/"}>
 				<img
-					className={styles.alchemy_logo}
-					src="/alchemy_logo.svg"
+					className={styles.logo_text}
+					src="/logo_text.svg"
 				></img>
 			</a>
 			<ConnectButton></ConnectButton>
